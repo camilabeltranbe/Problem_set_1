fcefc8114f56d36538c6bc9ae0a79fb007af3188
JuanCardona9905 <jp.rodriguezc12@uniandes.edu.co>
2024-02-24 18:04:02 -0500

    org

--- scripts/02_Data.R
+++ scripts/02_Data.R
@@ -32,6 +32,7 @@ view(head(data_tibble))
 }
 ########## Selección de variables e imputación de Missing values ##########
 {
+  ## a. Seleccion de variables
   ## Seleccion de variables con las cuales vamos a trabjar 
   data_tibble<- data_tibble %>% 
     select(directorio, secuencia_p, orden, estrato1, sex, age, ocu, oficio, orden, totalHoursWorked,
@@ -39,8 +40,10 @@ view(head(data_tibble))
            ingtotes,ingtotob, y_salary_m, y_total_m, hoursWorkUsual) 
   #view(head(data_tibble))
   # Se eliminaron 157 variables de la muestra inicial, dejando la nueva muestra con 22 variables.
-  
-  ## Filto de observaciones
+}
+
+{
+  ## b. Filto de observaciones
   # "In this problem set, we will focus only on employed individuals older than eighteen (18) years old.
   # 
   table(data$dominio) ## Solo se está trabajando con individuos de Bogota
@@ -49,7 +52,10 @@ view(head(data_tibble))
              ocu==1) ## Empleados
   # Se eliminaron 15.635 observaciones (aprox. el 49%) de la muestra inicial, dejando 16.542 observaciones.
   #view(data_tibble)
-  
+}
+
+{
+  ## c. Missing values
   sum(is.na(data_tibble$y_salary_m))
   sum(is.na(data_tibble$y_total_m))
   
@@ -72,10 +78,6 @@ view(head(data_tibble))
   data_tibble <- data_tibble %>%
     filter(!is.na(sal_imputado))
   
-  #Ahora bien,queremos crear la variable Female, para ello tomaremos la variable sex 
-  #que se encuentra en la encuesta
-  data_tibble$female <- 1 - data_tibble$sex
-  
   #creamos el logaritmo del salario mensual
   data_tibble$log_w=log(data_tibble$sal_imputado)
   
@@ -83,6 +85,15 @@ view(head(data_tibble))
   sum(is.nan(data_tibble$log_w))
 }
 
+{
+## d. Nuevas variables
+  #Ahora bien,queremos crear la variable Female, para ello tomaremos la variable sex 
+  #que se encuentra en la encuesta
+  data_tibble$female <- 1 - data_tibble$sex
+}
+
+
+any(is.na(data_tibble$regSalud)) #observemos si existen Missing values en la variable salario
 ####################### Estadisticas descriptivas ###########################
 {
 ## Pasar los datos a dataframe
