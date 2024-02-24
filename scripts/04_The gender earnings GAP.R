ffeef7a1c3c66511328ad3709e2d8bb3be071c3c
Camila Beltran <camilabeltranbe@gmail.com>
2024-02-23 23:25:12 -0500

    Update 04_The gender earnings GAP.R

--- scripts/04_The gender earnings GAP.R
+++ scripts/04_The gender earnings GAP.R
@@ -24,7 +24,7 @@ setwd(paste0(wd,"/stores"))
 load("data_GEIH.RData")
 
 data_tibble <- as_tibble(data) 
-view(head(data_tibble))
+#view(head(data_tibble))
 
 ########## Selección de variables e imputación de Missing values ##########
 {
@@ -33,7 +33,7 @@ data_tibble<- data_tibble %>%
   select(directorio, secuencia_p, orden, estrato1, sex, age, ocu, oficio, orden, totalHoursWorked,
          dsi, ie , formal, informal, sizeFirm , regSalud, maxEducLevel, ingtot,
          ingtotes,ingtotob, y_salary_m, y_total_m, hoursWorkUsual) 
-view(head(data_tibble))
+#view(head(data_tibble))
 # Se eliminaron 157 variables de la muestra inicial, dejando la nueva muestra con 21 variables.
 
 ## Filto de observaciones
@@ -44,7 +44,10 @@ data_tibble <- data_tibble %>%
   filter(age>=18 & ## Mayores de edad
            ocu==1) ## Empleados
 # Se eliminaron 15.635 observaciones (aprox. el 49%) de la muestra inicial, dejando 16.542 observaciones.
-view(data_tibble)
+#view(data_tibble)
+
+sum(is.na(data_tibble$y_salary_m))
+sum(is.na(data_tibble$y_total_m))
 
 #Ahora deseamos imputar los datos de aquellos individuos que se encuntran ocupados,
 #pero no reportan su ingreso.
@@ -78,7 +81,6 @@ sum(is.nan(data_tibble$log_w))
 ####################### a) Regresión ######################################
 {
 earnings_gap <- lm(log_w ~ female, data = data_tibble)
-
 # Mostrar resultados con stargazer
 stargazer(earnings_gap, type = "text") #quitar el "text" si se quiere en LATEX
 }
@@ -107,8 +109,6 @@ stargazer(equal_pay_fwl,type = "text",omit = "Constant",dep.var.labels = "Ln(sal
 stargazer(equal_pay_fwl,earnings_gap,type = "text",dep.var.labels = c("Ln(salario) con controles","Ln(salario) sin controles"),covariate.labels = "mujer")
 
 # ii) FWL- Bootstrap
-
-###########Opcion 1 - sin control ofico #############
 # funcion para realizar bootstrap
 female_fn<-function(data,index){
   res1 <- residuals(lm(log_w ~ age + maxEducLevel + hoursWorkUsual + oficio_factor, data = data_tibble, subset=index)) 
@@ -125,16 +125,26 @@ boot_r
 }
 
 ####### c) plot age-wage profile / estimate the implied “peak ages”########
-{
+
 #Creamos variable de edad^2
 data_tibble$age2 <- (data_tibble$age)^2
 
 #Realizamos el age-wage profile para mujeres
-# Crear una nueva base de datos solo con observaciones de mujeres
+#Crear una nueva base de datos solo con observaciones de mujeres
 female_data_tibble <- subset(data_tibble, sex == 0)
+age_wage_female <- lm(log_w ~ age + age2, data = female_data_tibble) 
+stargazer(age_wage_female, type = "text",omit = "Constant")
 
+#Realiza predicciones con el modelo
+female_data_tibble$predicted <- predict(age_wage_female, newdata = female_data_tibble)
+
+#Intervalos de confianza con bootstrap
 female_fn<-function(data,index){
- coef(lm(log_w ~ age + age2, data = female_data_tibble, subset= index))[1:3] #retorna el segundo coeficiente de la regresion
+  f <- lm(log_w ~ age + age2, data = female_data_tibble, subset= index)
+  b1 <- f$coefficients[2]
+  b2 <- f$coefficients[3]
+  max_edad <- -(b1)/(2*b2)
+  return(max_edad)
 }
 
 #Verifiquemos que la función... funciona!
@@ -142,24 +152,15 @@ female_fn(female_data_tibble,1:nrow(female_data_tibble))
 
 #Se utiliza la funcion boot para estimar la regresion con bootstrap
 set.seed(5382)
-boot_r <- boot(female_data_tibble, female_fn, R = 10)
-boot_r$t0
-
-# Realiza predicciones con el modelo e intervalos de confianza
-female_data_tibble <- female_data_tibble[,c("age","log_w","age2")]
-female_data_tibble$predicted <- boot_r$t0[1] + boot_r$t0[2]*female_data_tibble$age + boot_r$t0[3]*female_data_tibble$age2
-female_data_tibble$conf.low <- unlist(boot.ci(boot_r, type = "norm", index = 1)[4])[2] +
-                               unlist(boot.ci(boot_r, type = "norm", index = 2)[4])[2]*female_data_tibble$age +
-                               unlist(boot.ci(boot_r, type = "norm", index = 3)[4])[2]*female_data_tibble$age2
-female_data_tibble$conf.high <- unlist(boot.ci(boot_r, type = "norm", index = 1)[4])[3] +
-                                unlist(boot.ci(boot_r, type = "norm", index = 2)[4])[3]*female_data_tibble$age + 
-                                unlist(boot.ci(boot_r, type = "norm", index = 3)[4])[3]*female_data_tibble$age2
+boot_rf <- boot(female_data_tibble, female_fn, R = 1000)
+boot_rf <- as.data.frame(boot_rf$t)
+hist(boot_rf$V1) #distribucion del valor maximo de la edad con bootstrap
+quantile(boot_rf$V1,0.025) #percentil 2.5 (42.53374)
+quantile(boot_rf$V1,0.975) #percentil 97.5 (44.71811)
 
 female_plot <- ggplot(female_data_tibble, aes(x = age, y = log_w)) +
                 geom_point(aes(color = "Real"), alpha = 0.5) +  # Puntos para valores reales
                 geom_line(aes(y = predicted, color = "Predicho"), size = 1) +  # Línea para valores predichos
-                geom_ribbon(aes(ymin = conf.low, ymax = conf.high, fill = "Intervalo de Confianza"), 
-                            fill = "blue", alpha = 0.3) +  # Intervalos de confianza
                 scale_color_manual(values = c("Real" = "gray", "Predicho" = "darkblue"),name="") +  # Colores de puntos y líneas
                 labs(title = "Perfil edad-ingreso: mujeres",
                      x = "Edad",
@@ -168,57 +169,59 @@ female_plot <- ggplot(female_data_tibble, aes(x = age, y = log_w)) +
                 theme(plot.background = element_rect(fill = "white")) # Establecer el fondo del gráfico como blanco
 print(female_plot)
 
-# Realizamos el age-wage profile para hombres
-age_wage_male <- lm(log_w ~ age + age2, data = data_tibble, subset= sex==1) 
+#Realizamos el age-wage profile para hombres
+#Crear una nueva base de datos solo con observaciones de hombres
+male_data_tibble <- subset(data_tibble, sex == 1)
+age_wage_male <- lm(log_w ~ age + age2, data = male_data_tibble) 
 stargazer(age_wage_male, type = "text",omit = "Constant")
 
-# Crear una nueva base de datos solo con observaciones de hombres
-male_data_tibble <- subset(data_tibble, sex == 1)
+#Realiza predicciones con el modelo
+male_data_tibble$predicted <- predict(age_wage_male, newdata = male_data_tibble)
 
-# Realiza predicciones con el modelo e intervalos de confianza
-male_data_tibble$predicted <- predict(age_wage_male, newdata = male_data_tibble, interval = "confidence", level = 0.95)[,"fit"]
-male_data_tibble$conf.low <- predict(age_wage_male, newdata = male_data_tibble, interval = "confidence", level = 0.95)[,"lwr"]
-male_data_tibble$conf.high <- predict(age_wage_male, newdata = male_data_tibble, interval = "confidence", level = 0.95)[,"upr"]
+#Intervalos de confianza con bootstrap
+male_fn<-function(data,index){
+  f <- lm(log_w ~ age + age2, data = male_data_tibble, subset= index)
+  b1 <- f$coefficients[2]
+  b2 <- f$coefficients[3]
+  max_edad <- -(b1)/(2*b2)
+  return(max_edad)
+}
+
+#Verifiquemos que la función... funciona!
+male_fn(male_data_tibble,1:nrow(male_data_tibble))
+
+#Se utiliza la funcion boot para estimar la regresion con bootstrap
+set.seed(5382)
+boot_rm <- boot(male_data_tibble, male_fn, R = 1000)
+boot_rm <- as.data.frame(boot_rm$t)
+hist(boot_rm$V1) #distribucion del valor maximo de la edad con bootstrap
+quantile(boot_rm$V1,0.025) #percentil 2.5 (49.91383)
+quantile(boot_rm$V1,0.975) #percentil 97.5 (53.45359)
 
 male_plot <- ggplot(male_data_tibble, aes(x = age, y = log_w)) +
-             geom_point(aes(color = "Real"), alpha = 0.5) +  # Puntos para valores reales
-             geom_line(aes(y = predicted, color = "Predicho"), size = 1) +  # Línea para valores predichos
-             geom_ribbon(aes(ymin = conf.low, ymax = conf.high, fill = "Intervalo de Confianza"), 
-             fill = "red", alpha = 0.3) +  # Intervalos de confianza
-             scale_color_manual(values = c("Real" = "gray", "Predicho" = "darkred"),name="") +  # Colores de puntos y líneas
-             labs(title = "Perfil edad-ingreso: hombres",
-               x = "Edad",
-               y = "Ln(salario)") +
-             theme_minimal() +
-             theme(plot.background = element_rect(fill = "white")) # Establecer el fondo del gráfico como blanco
-
+              geom_point(aes(color = "Real"), alpha = 0.5) +  # Puntos para valores reales
+              geom_line(aes(y = predicted, color = "Predicho"), size = 1) +  # Línea para valores predichos
+              scale_color_manual(values = c("Real" = "gray", "Predicho" = "darkred"),name="") +  # Colores de puntos y líneas
+              labs(title = "Perfil edad-ingreso: hombres",
+                   x = "Edad",
+                   y = "Ln(salario)") +
+              theme_minimal() +
+              theme(plot.background = element_rect(fill = "white")) # Establecer el fondo del gráfico como blanco
 print(male_plot)
 
-# Edad maximizadora mujeres
-female_age_max <- -(age_wage_female$coefficients["age"])/(2*age_wage_female$coefficients["age2"])
-female_age_max # Resulta ser a los 44 años
-View(female_data_tibble[female_data_tibble$age==44,c("predicted","conf.low","conf.high")])
-
-# Edad maximizadora hombres
-male_age_max <- -(age_wage_male$coefficients["age"])/(2*age_wage_male$coefficients["age2"])
-male_age_max # Resulta ser a los 51 años
-View(male_data_tibble[male_data_tibble$age==51,c("predicted","conf.low","conf.high")])
-
+#plot de perfil edad - ingreso por sexo
 predicted_data <- rbind(female_data_tibble,male_data_tibble)
 predicted_data$sex <- factor(predicted_data$sex, levels = c(0, 1), labels = c("Mujeres", "Hombres"))
 
 # Plot de prediccion por edad y sexo
 gender_plot <- ggplot(predicted_data, aes(x = age, y = predicted, color = sex)) +
                 geom_line() +  # Líneas continuas
-                geom_ribbon(aes(ymin = conf.low, ymax = conf.high, fill = sex),show.legend = F, alpha = 0.3) +  # Banda de intervalo de confianza
                 labs(title = "Ingreso por edad y sexo",
-                   x = "Edad",
-                   y = "Ingreso") +
+                     x = "Edad",
+                     y = "Ingreso") +
                 theme_minimal() +
                 scale_color_manual(values = c("Mujeres" = "darkblue", "Hombres" = "darkred"),name="") + # Cambiar colores de las líneas
-                scale_fill_manual(values = c("Mujeres" = "lightblue", "Hombres" = "pink")) + # Cambiar colores del relleno
                 theme(plot.background = element_rect(fill = "white")) # Establecer el fondo del gráfico como blanco
-
 print(gender_plot)
 
 #Guardar los graficos
@@ -226,24 +229,3 @@ setwd(paste0(wd,"/Views"))
 ggsave("age_wage_profile_hombres.png", male_plot, width = 10, height = 6, units = "in")
 ggsave("age_wage_profile_mujeres.png", female_plot, width = 10, height = 6, units = "in")
 ggsave("age_wage_profile_gender.png", gender_plot, width = 10, height = 6, units = "in")
-
-
-#Solición propuesta 1
-#Creamos variable de edad^2
-data_tibble$age2 <- (data_tibble$age)^2
-# Ajustar el modelo de regresión lineal
-female_gap_reg <- lm(log_w ~ age + age2, data = data_tibble)
-view(female_gap_predicted)
-# Predecir valores usando el modelo
-female_gap_predicted <- predict(female_gap_reg)
-
-max_value <- max(female_gap_predicted)
-
-n_iterations <- 1000
-
-boot_max<-numeric(n_iterations)
-
-for(i in 1:n_iterations){data_tibble<-female_gap_predicted[sample(nrow(female_gap_predicted),replace=TRUE), ]
-boot_max[i]<-max(data_tibble$X)}  
-
-}
