fbdc828918ed31fbe672f2596556c7eb2e7df3d5
Camila Beltran <camilabeltranbe@gmail.com>
2024-03-03 20:40:15 -0500

    Update 05_Predicting_Earnings.R

--- scripts/05_Predicting_Earnings.R
+++ scripts/05_Predicting_Earnings.R
@@ -2,31 +2,10 @@
 ########### Punto 5 -Problem set ################
 #################################################
 
-rm(list = ls()) #se borra la memoria
-library(pacman) 
-p_load(tidyverse, # tidy-data (ggplot y Tidyverse)
-       skimr, # summary data
-       visdat, ## visualizing missing data
-       corrplot, ## Correlation Plots 
-       stargazer, # tables/output to TEX.
-       readxl, # importar Excel
-       writexl, # exportar Excel
-       rio, # import/export data
-       caret, # For predictive model assessment
-       gridExtra, ## visualizing missing data
-       data.table,
-       boot) ## bootstrapping
-
-#cambiar esta ruta por el directorio de cada uno
-ifelse(getwd()=="/Users/camilabeltran",
-       wd <- "/Users/camilabeltran/OneDrive/Educación/PEG - Uniandes/BDML/GitHub/problem_set/Problem_set_1",
-       wd <- "/Users/aleja/Documents/Maestría Uniandes/Clases/Big Data y Machine Learning/Repositorios Git Hub/Problem_set_1/")
-
-#A continuación, encontrarán el código realizado para llegar a los resultados que se encuentran cargados en la imagen:
-
 #cargar la base de datos a través de image 
-setwd(paste0(wd,"/stores"))
-load("gender_gap_earnings.R")
+#setwd(paste0(wd,"/stores"))
+#load("04_gender_gap_earnings.R")
+
 #La base cargada previamente contiene estimaciones correspondientes al punto anterior:
 #Como la regresión lineal de log_w y female sin controles. Asimismo se tiene la estimación
 # con controles como: 
@@ -614,6 +593,3 @@ ggplot(scores_modelos_menor_error, aes(x = Model, y = RMSE, col = Approach)) +
 
 setwd(paste0(wd,"/views"))
 write.table(scores_modelos_menor_error, "scores_modelos_menor_error.txt", sep = "\t", row.names = FALSE)
-
-#setwd(paste0(wd,"/stores"))
-#save.image("05_predicting_earnings.RData")
